e1b12f2bdf9e21863e536c461fdd4367132519de
dineshkarde103 <dineshkarde103@gmail.com>
2023-05-28 13:14:37 +0530

    update

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,7 @@ const { default: mongoose } = require("mongoose")
 
 const dbConnect = () =>{
   console.log('Database conected!!!')
-  return mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+  return mongoose.connect('mongodb+srv://dinesh103:dinesh103DINESH103@cluster0.3pvw9hk.mongodb.net/Bank?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 }
 
 module.exports=dbConnect;
